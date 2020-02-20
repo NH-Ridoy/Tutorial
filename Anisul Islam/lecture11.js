7beb39bdb779c59b7eca1cd4e52d11ba5bed3e40
NH <ridoyhossain5557@gmail.com>
2020-02-20 06:14:07 -0800

    Another Testing

---
+++ Anisul Islam/lecture11.js
@@ -0,0 +1,16 @@
+// var farenheite = prompt('Enter farenheite number : ');
+
+// var celsius = (farenheite-32) * (5/9);
+
+// document.write('Celsius : ' + celsius);
+
+
+// relational operator ->     > >= < <= == === != !==
+// logical operator -> && || !
+
+
+
+
+
+
+
---
+++ OOp/lecture3.js
@@ -0,0 +1,18 @@
+// obj = {
+//     name: 'Micbael Fogus',
+
+//     print: function() {
+//         console.log(this);
+//     }
+// }
+
+// obj.print();
+
+// function myFunc() {
+//     function inner() {
+//         console.log(this);
+//     }
+//     new inner();
+// }
+
+// myFunc();
